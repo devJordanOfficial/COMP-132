1d2be9a8da850f76c233670408c89db74c4e6a89
devJordanOfficial <94763582+devJordanOfficial@users.noreply.github.com>
2022-04-11 16:06:41 -0700

    Create Student.java

---
+++ lab9/Student.java
@@ -0,0 +1,31 @@
+package me.jordan.lab9;
+
+public class Person {
+
+    public final String ID;
+    public String firstName;
+    public String lastName;
+
+    public Person(String first, String last) {
+        this(first, last, first+last);
+    }
+
+    public Person(String first, String last, String id) {
+        ID = id;
+        firstName = first;
+        lastName = last;
+    }
+
+    public void sayHelloTo(String name) {
+        System.out.println("Hey " + name + "! My name is " + firstName + ".");
+    }
+
+    public void sayHelloTo(Person person) {
+        sayHelloTo(person.firstName);
+    }
+
+    @Override
+    public String toString() {
+        return "Person: " + firstName + " " + lastName + " (" + ID + ")";
+    }
+}
